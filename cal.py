0ce23aee84e2ac73702f7ff1fb26871025bc8cb2
Sandeep Reddy <103351604+SandeepReddypulgam@users.noreply.github.com>
2022-05-09 15:14:35 +0530

    Delete cal.py

--- cal.py
+++
@@ -1,9 +0,0 @@
-class Rational:
-    def add(self, x, y): 
-         return x + y
-    def subtract(self, x, y):
-        return x - y
-    def divide(self, x, y):
-        return x / y
-    def multiply(self, x, y):
-        return x * y
