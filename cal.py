dac6c7ce4c378ecac88e8e1173b6358645d363ac
Sandeep Reddy <103351604+SandeepReddypulgam@users.noreply.github.com>
2022-05-09 14:56:09 +0530

    Update cal.py

--- cal.py
+++ cal.py
@@ -1,21 +1,9 @@
-print ("Enter Number 1:")
-num1 = int(input())
- 
-print ("Enter Number 2:")
-num2 = int(input())
- 
-#Addition
-add = num1 + num2
-print ("Addition: %d" %add)
- 
-#Subtraction
-sub = num1 - num2
-print ("Subtraction: %d" %sub)
- 
-#Multiplication
-mul = num1 * num2
-print ("Multiplication: %d" %mul)
- 
-#Division
-div = num1 / num2
-print ("Division: %.2f" %div)
+class Rational:
+    def add(self, x, y): 
+         return x + y
+    def subtract(self, x, y):
+        return x - y
+    def divide(self, x, y):
+        return x / y
+    def multiply(self, x, y):
+        return x * y
